f08350400af7c0bb8299b5d366f0b8f04d1e2afb
JihunKwon <jihun.k@frontier.hokudai.ac.jp>
2019-06-27 14:56:15 +0900

    implemented training&test set split for before phase

--- show_meanSD_fill.py
+++ show_meanSD_fill.py
@@ -16,57 +16,63 @@ sr_list = ['s1r1', 's1r2', 's2r1', 's2r2', 's3r1', 's3r2']
 #sr_list = ['s1r1']
 for fidx in range(0,np.size(sr_list)):
     Sub_run = sr_list[fidx]
-    sr_name = 'ocm012_' + Sub_run + '.pkl'  # filtered
-    #sr_name = 'Raw_det_ocm012_' + Sub_run + '.pkl'  # raw
+    #sr_name = 'ocm012_' + Sub_run + '.pkl'  # filtered
+    sr_name = 'Raw_det_ocm012_' + Sub_run + '.pkl'  # raw
 
     with open(sr_name, 'rb') as f:
-        ocm0_all_r1, ocm1_all_r1, ocm2_all_r1 = pickle.load(f)
+        ocm0_all, ocm1_all, ocm2_all = pickle.load(f)
 
-    print(ocm0_all_r1.shape)
-    depth = np.linspace(0, ocm0_all_r1.shape[0] - 1, ocm0_all_r1.shape[0])
-    num_max = ocm0_all_r1.shape[1]  # Total num of traces
+    print(ocm0_all.shape)
+    depth = np.linspace(0, ocm0_all.shape[0] - 1, ocm0_all.shape[0])
+    num_max = ocm0_all.shape[1]  # Total num of traces
 
-    '''
-    # Calculate mean and sd
-    ocm0_m = np.zeros([ocm0_all_r1.shape[0], 2]) # 2rd dimension is before and after water
-    ocm0_sd = np.zeros([ocm0_all_r1.shape[0], 2])
-    ocm1_m = np.zeros([ocm0_all_r1.shape[0], 2])
-    ocm1_sd = np.zeros([ocm0_all_r1.shape[0], 2])
-    ocm2_m = np.zeros([ocm0_all_r1.shape[0], 2])
-    ocm2_sd = np.zeros([ocm0_all_r1.shape[0], 2])
-    
-    # Case1. Use all bh to calculate mean
-    ocm0_m = np.mean(ocm0_all_r1[:,:,:], axis=1)
-    ocm0_sd = np.std(ocm0_all_r1[:,:,:], axis=1)
-    ocm1_m = np.mean(ocm1_all_r1[:,:,:], axis=1)
-    ocm1_sd = np.std(ocm1_all_r1[:,:,:], axis=1)
-    ocm2_m = np.mean(ocm2_all_r1[:,:,:], axis=1)
-    ocm2_sd = np.std(ocm2_all_r1[:,:,:], axis=1)
-    '''
+    # Split data to "before, train", "before, test", and "after, test".
+    # first, get "before" components
+    ocm_bef = np.zeros([ocm0_all.shape[0], ocm0_all.shape[1], 3])
+    ocm_aft = np.zeros([ocm0_all.shape[0], ocm0_all.shape[1], 3])
+    ocm_bef[:, :, 0] = ocm0_all[:, :, 0]
+    ocm_bef[:, :, 1] = ocm1_all[:, :, 0]
+    ocm_bef[:, :, 2] = ocm2_all[:, :, 0]
+    ocm_aft[:, :, 0] = ocm0_all[:, :, 1]
+    ocm_aft[:, :, 1] = ocm1_all[:, :, 1]
+    ocm_aft[:, :, 2] = ocm2_all[:, :, 1]
+    print('ocm_bef', ocm_bef.shape)
+    print('ocm_aft', ocm_aft.shape)
+    # Transpose
+    ocm_bef = np.einsum('abc->bac', ocm_bef)
+    ocm_aft = np.einsum('abc->bac', ocm_aft)
+    print('ocm_bef', ocm_bef.shape)
+    print('ocm_aft', ocm_aft.shape)
+    # Shuffle "before"
+    np.random.shuffle(ocm_bef)
 
-    # Case2. Use bh 1~4 to calculate mean (bh5 is used to )
-    ocm0_m_2 = np.zeros([1, ocm0_all_r1.shape[0]])  # 2rd dimension is before and after water
-    ocm0_sd_2 = np.zeros([1, ocm0_all_r1.shape[0]])
-    ocm1_m_2 = np.zeros([1, ocm0_all_r1.shape[0]])
-    ocm1_sd_2 = np.zeros([1, ocm0_all_r1.shape[0]])
-    ocm2_m_2 = np.zeros([1, ocm0_all_r1.shape[0]])
-    ocm2_sd_2 = np.zeros([1, ocm0_all_r1.shape[0]])
+    ratio = 0.2  # ratio of test set for before
+    ocm_bef_train = ocm_bef[0:int(num_max*(1-ratio)), :, :]  # First 80% traces are for training
+    ocm_bef_test = ocm_bef[int(num_max*(1-ratio)):num_max, :, :]  # Last 20% traces are for training
+    print('ocm_bef_train:', ocm_bef_train.shape)
+    print('ocm_bef_test:', ocm_bef_test.shape)
 
-    # Calculate mean of bh1~4 for "before" water phase
-    '''
-    ocm0_m_2 = np.mean(ocm0_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    ocm0_sd_2 = np.std(ocm0_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    ocm1_m_2 = np.mean(ocm1_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    ocm1_sd_2 = np.std(ocm1_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    ocm2_m_2 = np.mean(ocm2_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    ocm2_sd_2 = np.std(ocm2_all_r1[:, 0:int(num_max/5*4), 0], axis=1)
-    '''
-    ocm0_m_2 = np.mean(ocm0_all_r1[:, 0:num_max, 0], axis=1)
-    ocm0_sd_2 = np.std(ocm0_all_r1[:, 0:num_max, 0], axis=1)
-    ocm1_m_2 = np.mean(ocm1_all_r1[:, 0:num_max, 0], axis=1)
-    ocm1_sd_2 = np.std(ocm1_all_r1[:, 0:num_max, 0], axis=1)
-    ocm2_m_2 = np.mean(ocm2_all_r1[:, 0:num_max, 0], axis=1)
-    ocm2_sd_2 = np.std(ocm2_all_r1[:, 0:num_max, 0], axis=1)
+    # Calculate mean of "before test"
+    ocm_bef_m = np.zeros([ocm_bef_train.shape[1], 3])
+    ocm_bef_sd = np.zeros([ocm_bef_train.shape[1], 3])
+
+    ocm_bef_m[:, 0] = np.mean(ocm_bef_train[:, :, 0], axis=0)
+    ocm_bef_m[:, 1] = np.mean(ocm_bef_train[:, :, 1], axis=0)
+    ocm_bef_m[:, 2] = np.mean(ocm_bef_train[:, :, 2], axis=0)
+    ocm_bef_sd[:, 0] = np.std(ocm_bef_train[:, :, 0], axis=0)
+    ocm_bef_sd[:, 1] = np.std(ocm_bef_train[:, :, 1], axis=0)
+    ocm_bef_sd[:, 2] = np.std(ocm_bef_train[:, :, 2], axis=0)
+
+    # Calculate mean of "after test"
+    ocm_aft_m = np.zeros([ocm_aft.shape[1], 3])
+    ocm_aft_sd = np.zeros([ocm_aft.shape[1], 3])
+
+    ocm_aft_m[:, 0] = np.mean(ocm_aft[:, :, 0], axis=0)
+    ocm_aft_m[:, 1] = np.mean(ocm_aft[:, :, 1], axis=0)
+    ocm_aft_m[:, 2] = np.mean(ocm_aft[:, :, 2], axis=0)
+    ocm_aft_sd[:, 0] = np.std(ocm_aft[:, :, 0], axis=0)
+    ocm_aft_sd[:, 1] = np.std(ocm_aft[:, :, 1], axis=0)
+    ocm_aft_sd[:, 2] = np.std(ocm_aft[:, :, 2], axis=0)
 
     # ========================Visualize==============================================
     '''
@@ -74,117 +80,113 @@ for fidx in range(0,np.size(sr_list)):
     # This part shows raw signals
     ## Mean+1SD
     # OCM0
+    ocm = 0
     ax0 = fig.add_subplot(231)
-    a0 = ax0.plot(depth, ocm0_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm0_all_r1.shape[0]), ocm0_m[:,0]-ocm0_sd[:,0], ocm0_m[:,0]+ocm0_sd[:,0], alpha=.3)
-    a1 = ax0.plot(depth, ocm0_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm0_all_r1.shape[0]), ocm0_m[:,1]-ocm0_sd[:,1], ocm0_m[:,1]+ocm0_sd[:,1], alpha=.3)
+    a0 = ax0.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax0.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+ocm_aft_sd[:,ocm], alpha=.3)
     ax0.set_title('OCM0, 1SD')
-    ax0.set_ylim(-2,2)
+    ax0.set_ylim(-1,1)
     ax0.set_xlabel('Depth')
     ax0.set_ylabel('Intensity')
     plt.legend(loc='best')
     # OCM1
+    ocm = 1
     ax1 = fig.add_subplot(232)
-    a0 = ax1.plot(depth, ocm1_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm1_all_r1.shape[0]), ocm1_m[:,0]-ocm1_sd[:,0], ocm1_m[:,0]+ocm1_sd[:,0], alpha=.3)
-    a1 = ax1.plot(depth, ocm1_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm1_all_r1.shape[0]), ocm1_m[:,1]-ocm1_sd[:,1], ocm1_m[:,1]+ocm1_sd[:,1], alpha=.3)
+    a0 = ax1.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax1.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+ocm_aft_sd[:,ocm], alpha=.3)
     ax1.set_title('OCM1, 1SD')
-    ax1.set_ylim(-2,2)
+    ax1.set_ylim(-1,1)
+    ax1.set_xlabel('Depth')
     # OCM2
+    ocm = 2
     ax2 = fig.add_subplot(233)
-    a0 = ax2.plot(depth, ocm2_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm2_all_r1.shape[0]), ocm2_m[:,0]-ocm2_sd[:,0], ocm2_m[:,0]+ocm2_sd[:,0], alpha=.3)
-    a1 = ax2.plot(depth, ocm2_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm2_all_r1.shape[0]), ocm2_m[:,1]-ocm2_sd[:,1], ocm2_m[:,1]+ocm2_sd[:,1], alpha=.3)
+    a0 = ax2.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax2.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+ocm_aft_sd[:,ocm], alpha=.3)
     ax2.set_title('OCM2, 1SD')
-    ax2.set_ylim(-2,2)
+    ax2.set_ylim(-1,1)
+    ax2.set_xlabel('Depth')
 
     ## Mean+3SD
     # OCM0
+    ocm = 0
     ax0 = fig.add_subplot(234)
-    a0 = ax0.plot(depth, ocm0_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm0_all_r1.shape[0]), ocm0_m[:,0]-3*ocm0_sd[:,0], ocm0_m[:,0]+3*ocm0_sd[:,0], alpha=.3)
-    a1 = ax0.plot(depth, ocm0_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm0_all_r1.shape[0]), ocm0_m[:,1]-3*ocm0_sd[:,1], ocm0_m[:,1]+3*ocm0_sd[:,1], alpha=.3)
+    a0 = ax0.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-3*ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+3*ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax0.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-3*ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+3*ocm_aft_sd[:,ocm], alpha=.3)
     ax0.set_title('OCM0, 3SD')
     ax0.set_ylim(-2,2)
     ax0.set_xlabel('Depth')
     ax0.set_ylabel('Intensity')
     plt.legend(loc='best')
     # OCM1
+    ocm = 1
     ax1 = fig.add_subplot(235)
-    a0 = ax1.plot(depth, ocm1_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm1_all_r1.shape[0]), ocm1_m[:,0]-3*ocm1_sd[:,0], ocm1_m[:,0]+3*ocm1_sd[:,0], alpha=.3)
-    a1 = ax1.plot(depth, ocm1_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm1_all_r1.shape[0]), ocm1_m[:,1]-3*ocm1_sd[:,1], ocm1_m[:,1]+3*ocm1_sd[:,1], alpha=.3)
+    a0 = ax1.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-3*ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+3*ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax1.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-3*ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+3*ocm_aft_sd[:,ocm], alpha=.3)
     ax1.set_title('OCM1, 3SD')
     ax1.set_ylim(-2,2)
+    ax1.set_xlabel('Depth')
     # OCM2
+    ocm = 2
     ax2 = fig.add_subplot(236)
-    a0 = ax2.plot(depth, ocm2_m[:, 0], 'b', linewidth=2, label="Before")  # Before water
-    plt.fill_between(range(ocm2_all_r1.shape[0]), ocm2_m[:,0]-3*ocm2_sd[:,0], ocm2_m[:,0]+3*ocm2_sd[:,0], alpha=.3)
-    a1 = ax2.plot(depth, ocm2_m[:, 1], 'r', linewidth=2, label="After")  # After water
-    plt.fill_between(range(ocm2_all_r1.shape[0]), ocm2_m[:,1]-3*ocm2_sd[:,1], ocm2_m[:,1]+3*ocm2_sd[:,1], alpha=.3)
+    a0 = ax2.plot(depth, ocm_bef_m[:, ocm], 'b', linewidth=2, label="Before")  # Before water
+    plt.fill_between(range(ocm_bef_m.shape[0]), ocm_bef_m[:,ocm]-3*ocm_bef_sd[:,ocm], ocm_bef_m[:,ocm]+3*ocm_bef_sd[:,ocm], alpha=.3)
+    a1 = ax2.plot(depth, ocm_aft_m[:, ocm], 'r', linewidth=2, label="After")  # After water
+    plt.fill_between(range(ocm_aft_m.shape[0]), ocm_aft_m[:,ocm]-3*ocm_aft_sd[:,ocm], ocm_aft_m[:,ocm]+3*ocm_aft_sd[:,ocm], alpha=.3)
     ax2.set_title('OCM2, 3SD')
     ax2.set_ylim(-2,2)
+    ax2.set_xlabel('Depth')
 
     fig.tight_layout()
     fig.show()
     f_name = 'Mean_SD_' + Sub_run + '.png'
     plt.savefig(f_name)
     # =============================================================================
-    '''
 
+'''
     # ======================== Count num of traces outside of mean+3SD ==============================================
     ocm_bef_out = np.zeros([3, 6])
     ocm_aft_out = np.zeros([3, 6])
-    for num in range(0, num_max):  # each traces
+
+    # Before
+    for num in range(0, int(num_max * ratio)):  # each traces
         # If any of the depth is out of the envelope, flag will be 1.
-        flag_0_bef = 0
-        flag_1_bef = 0
-        flag_2_bef = 0
-        flag_0_aft = 0
-        flag_1_aft = 0
-        flag_2_aft = 0
-
+        flag_bef = [0, 0, 0]
         # Detect out of envelope
+        for d in range(0, ocm0_all.shape[0]):
+            for ocm in range(0, 3):
+                mean = ocm_bef_m[d, ocm]
+                sd = ocm_bef_sd[d, ocm]
+                # Before
+                if flag_bef[ocm] == 0:  # if no change has been detected in shallower region
+                    # if (before < mean-3SD) or (mean+3SD < before)
+                    if ((ocm_bef_test[num, d, ocm] < (mean - 3 * sd)) or ((mean + 3 * sd) < ocm_bef_test[num, d, ocm])):
+                        ocm_bef_out[ocm][fidx] = ocm_bef_out[ocm][fidx] + 1  # False Positive
+                        flag_bef[ocm] = 1  # Out of envelope!
 
-        for d in range(0, ocm0_all_r1.shape[0]):
-            ocm = 0
-            phase = 0  # Before
-            if flag_0_bef == 0:  # if no change has been detected in shallower region
-                if (ocm0_all_r1[d, num, phase] < (ocm0_m_2[d] - 3 * ocm0_sd_2[d])) or ((ocm0_m_2[d] + 3 * ocm0_sd_2[d]) < ocm0_all_r1[d, num, phase]):
-                    ocm_bef_out[ocm][fidx] = ocm_bef_out[ocm][fidx] + 1  # False Positive
-                    flag_0_bef = 1  # Change detected! This trace is done.
-            phase = 1 # After
-            if flag_0_aft == 0:
-                if (ocm0_all_r1[d, num, phase] < (ocm0_m_2[d] - 3 * ocm0_sd_2[d])) or ((ocm0_m_2[d] + 3 * ocm0_sd_2[d]) < ocm0_all_r1[d, num, phase]):
-                    ocm_aft_out[ocm][fidx] = ocm_aft_out[ocm][fidx] + 1  # True Positive
-                    flag_0_aft = 1  # Change detected! This trace is done.
-            ocm = 1
-            phase = 0  # Before
-            if flag_1_bef == 0:
-                if (ocm1_all_r1[d, num, phase] < (ocm1_m_2[d] - 3 * ocm1_sd_2[d])) or ((ocm1_m_2[d] + 3 * ocm1_sd_2[d]) < ocm1_all_r1[d, num, phase]):
-                    ocm_bef_out[ocm][fidx] = ocm_bef_out[ocm][fidx] + 1  # False Positive
-                    flag_1_bef = 1  # Change detected! This trace is done.
-            phase = 1 # After
-            if flag_1_aft == 0:
-                if (ocm1_all_r1[d, num, phase] < (ocm1_m_2[d] - 3 * ocm1_sd_2[d])) or ((ocm1_m_2[d] + 3 * ocm1_sd_2[d]) < ocm1_all_r1[d, num, phase]):
-                    ocm_aft_out[ocm][fidx] = ocm_aft_out[ocm][fidx] + 1  # True Positive
-                    flag_1_aft = 1  # Change detected! This trace is done.
-            ocm = 2
-            phase = 0  # Before
-            if flag_2_bef == 0:
-                if (ocm2_all_r1[d, num, phase] < (ocm2_m_2[d] - 3 * ocm2_sd_2[d])) or ((ocm2_m_2[d] + 3 * ocm2_sd_2[d]) < ocm2_all_r1[d, num, phase]):
-                    ocm_bef_out[ocm][fidx] = ocm_bef_out[ocm][fidx] + 1  # False Positive
-                    flag_2_bef = 1  # Change detected! This trace is done.
-            phase = 1 # After
-            if flag_2_aft == 0:
-                if (ocm2_all_r1[d, num, phase] < (ocm2_m_2[d] - 3 * ocm2_sd_2[d])) or ((ocm2_m_2[d] + 3 * ocm2_sd_2[d]) < ocm2_all_r1[d, num, phase]):
-                    ocm_aft_out[ocm][fidx] = ocm_aft_out[ocm][fidx] + 1  # True Positive
-                    flag_2_aft = 1  # Change detected! This trace is done.
+    # After
+    for num in range(0, num_max):  # each traces
+        flag_aft = [0, 0, 0]
+        # Detect out of envelope
+        for d in range(0, ocm0_all.shape[0]):
+            for ocm in range(0, 3):
+                mean = ocm_bef_m[d, ocm]
+                sd = ocm_bef_sd[d, ocm]
+                # After
+                if flag_aft[ocm] == 0:  # if no change has been detected in shallower region
+                    # if (after < mean-3SD) or (mean+3SD < after)
+                    if ((ocm_aft[num, d, ocm] < (mean - 3 * sd)) or ((mean + 3 * sd) < ocm_aft[num, d, ocm])):
+                        ocm_aft_out[ocm][fidx] = ocm_aft_out[ocm][fidx] + 1  # True Positive
+                        flag_aft[ocm] = 1  # Out of envelope!
 
     print('fidx:', Sub_run)
     TP = [0, 0, 0]
@@ -192,15 +194,20 @@ for fidx in range(0,np.size(sr_list)):
     TN = [0, 0, 0]
     FP = [0, 0, 0]
 
-    for ocm in range(0,3):
+    for ocm in range(0, 3):
         TP[ocm] = ocm_aft_out[ocm][fidx]  # After, change detected
         FN[ocm] = num_max - ocm_aft_out[ocm][fidx]  # After, change not detected
-        TN[ocm] = int(num_max / 1) - ocm_bef_out[ocm][fidx]  # Before, change not detected
-        FP[ocm] = ocm_bef_out[ocm][fidx]  # Before, change detected
+        TN[ocm] = int(num_max * ratio) - ocm_bef_out[ocm][fidx]  # Before "test", change not detected
+        FP[ocm] = ocm_bef_out[ocm][fidx]  # Before "test", change detected
 
-    print(Sub_run,', OCM', str(ocm))
-    #print('TP,FN,TN,FP: ', '{:.3f}'.format(TP), ' ', '{:.3f}'.format(FN), ' ', '{:.3f}'.format(TN), ' ', '{:.3f}'.format(FP))
-    print('TPR,FNR,Recall: ', '{:.3f}'.format(TP[0]/num_max), ' ', '{:.3f}'.format(FN[0]/num_max),
-          ' ', '{:.3f}'.format(TP[1]/num_max), ' ', '{:.3f}'.format(FN[1]/num_max),
-          ' ', '{:.3f}'.format(TP[2]/num_max), ' ', '{:.3f}'.format(FN[2]/num_max))
+    print('fidx:', Sub_run)
+#    print('TP,FN,TN,FP: ', '{:.3f}'.format(TP[0]), ' ', '{:.3f}'.format(FN[0]), ' ', '{:.3f}'.format(TN[0]), ' ', '{:.3f}'.format(FP[0])
+#          , ' ', '{:.3f}'.format(TP[1]), ' ', '{:.3f}'.format(FN[1]), ' ', '{:.3f}'.format(TN[1]), ' ', '{:.3f}'.format(FP[1])
+#          , ' ', '{:.3f}'.format(TP[2]), ' ', '{:.3f}'.format(FN[2]), ' ', '{:.3f}'.format(TN[2]), ' ', '{:.3f}'.format(FP[2]))
+    print('TPR,FNR,TNR,FPR: ', '{:.3f}'.format(TP[0]/num_max), ' ', '{:.3f}'.format(FN[0]/num_max), ' ', '{:.3f}'.format(TN[0]/int(num_max*ratio)), ' ', '{:.3f}'.format(FP[0]/int(num_max*ratio))
+          , ' ', '{:.3f}'.format(TP[1]/num_max), ' ', '{:.3f}'.format(FN[1]/num_max), ' ', '{:.3f}'.format(TN[1]/int(num_max*ratio)), ' ', '{:.3f}'.format(FP[1]/int(num_max*ratio))
+          , ' ', '{:.3f}'.format(TP[2]/num_max), ' ', '{:.3f}'.format(FN[2]/num_max), ' ', '{:.3f}'.format(TN[2]/int(num_max*ratio)), ' ', '{:.3f}'.format(FP[2]/int(num_max*ratio)))
+    #print('TPR,FNR,Recall: ', '{:.3f}'.format(TP[0]/num_max), ' ', '{:.3f}'.format(FN[0]/num_max),
+    #      ' ', '{:.3f}'.format(TP[1]/num_max), ' ', '{:.3f}'.format(FN[1]/num_max),
+    #      ' ', '{:.3f}'.format(TP[2]/num_max), ' ', '{:.3f}'.format(FN[2]/num_max))
     print('')
