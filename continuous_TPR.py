7e4fc22dae014cdf2401447e68447d447810b2b6
JihunKwon <jihun.k@frontier.hokudai.ac.jp>
2019-07-02 15:48:43 +0900

    fixed confusion of TPR and FPR in the before water phase

--- continuous_TPR.py
+++ continuous_TPR.py
@@ -13,9 +13,9 @@ plt.rcParams['font.family'] ='sans-serif'
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 
-sr_list = ['s1r1', 's1r2', 's2r1', 's2r2', 's3r1', 's3r2']
-#sr_list = ['s1r1']
-tole = 10  # tolerance level
+#sr_list = ['s1r1', 's1r2', 's2r1', 's2r2', 's3r1', 's3r2']
+sr_list = ['s1r1']
+tole = 0  # tolerance level
 
 bh_train = 2
 bh_test = 10 - bh_train
@@ -79,7 +79,7 @@ for fidx in range(0, np.size(sr_list)):
         ocm_train_sd[:, ocm] = np.std(ocm_train[:, :, ocm], axis=0)
 
     ## Check performance of "train" set
-    output_train = [0, 0, 0]
+    outside_train = [0, 0, 0]
     for num in range(0, bh * bh_train):  # each traces
         # If any of the depth is out of the envelope, flag will be 1.
         flag = [0, 0, 0]
@@ -93,17 +93,20 @@ for fidx in range(0, np.size(sr_list)):
                     if ((ocm_train[num, d, ocm] < (mean - 3 * sd)) or ((mean + 3 * sd) < ocm_train[num, d, ocm])):
                         flag[ocm] = flag[ocm] + 1  # Out of envelope!
                     if flag[ocm] > tole:
-                        output_train[ocm] = output_train[ocm] + 1  # False Positive
+                        outside_train[ocm] = outside_train[ocm] + 1  # Store outside of envelope
 
-    TP = np.zeros([9, 3])  # [bh_test, ocm]
-    FN = np.zeros([9, 3])
-    TN = np.zeros([9, 3])
-    FP = np.zeros([9, 3])
+    TP = np.zeros([10, 3])  # [bh_test, ocm]
+    FN = np.zeros([10, 3])
+    TN = np.zeros([10, 3])
+    FP = np.zeros([10, 3])
     ocm_bh_test = np.zeros([bh, depth, 3])
+    outside_test = np.zeros([bh_test, 3])
+
+    print('###### Test begins #####')
 
     # Calculate mean of "test" (each bh separately)
     for bh_cnt in range(0, bh_test):
-        output_test = [0, 0, 0]
+        #outside_test = [0, 0, 0]
         for ocm in range(0, 3):
             ocm_bh_test[:, :, ocm] = ocm_test[bh_cnt * bh:(bh_cnt + 1) * bh, :, ocm]
             ocm_test_m[:, ocm] = np.mean(ocm_test[bh_cnt * bh:(bh_cnt + 1) * bh, :, ocm], axis=0)
@@ -123,26 +126,45 @@ for fidx in range(0, np.size(sr_list)):
                         if ((ocm_bh_test[num, d, ocm] < (mean - 3 * sd)) or ((mean + 3 * sd) < ocm_bh_test[num, d, ocm])):
                             flag[ocm] = flag[ocm] + 1
                         if flag[ocm] > tole:
-                            output_test[ocm] = output_test[ocm] + 1  # True Positive
+                            outside_test[bh_cnt][ocm] = outside_test[bh_cnt][ocm] + 1  # Store outside of envelope
 
         # Gather each bh data
-        for ocm in range(0, 3):
-            TP[bh_cnt][ocm] = output_test[ocm]  # Test, change detected
-            FN[bh_cnt][ocm] = bh - output_test[ocm]  # Test, change not detected
-            TN[bh_cnt][ocm] = bh * bh_test - output_train[ocm]  # Train, change not detected
-            FP[bh_cnt][ocm] = output_train[ocm]  # Train, change detected
+        if 0 <= bh_cnt < (bh_test-5):  # if "before water"
+            for ocm in range(0, 3):
+                TN[bh_cnt][ocm] = bh - outside_test[bh_cnt][ocm]  # Before, change not detected
+                FP[bh_cnt][ocm] = outside_test[bh_cnt][ocm]  # Before, change detected
+        elif (bh_test-5) <= bh_cnt:  # if "after water"
+            for ocm in range(0, 3):
+                TP[bh_cnt][ocm] = outside_test[bh_cnt][ocm]  # After, change detected
+                FN[bh_cnt][ocm] = bh - outside_test[bh_cnt][ocm]  # After, change not detected
 
+    print('TP.shape:', TP.shape)
     print('fidx:', Sub_run)
     print('training set')
-    print('TN,FP: ', '{:.3f}'.format(TN[bh_cnt][0]), ' ', '{:.3f}'.format(FP[bh_cnt][0]),
-          '{:.3f}'.format(TN[bh_cnt][1]), ' ', '{:.3f}'.format(FP[bh_cnt][1]),
-          '{:.3f}'.format(TN[bh_cnt][2]), ' ', '{:.3f}'.format(FP[bh_cnt][2]))
-    print('TNR,FPR: ', '{:.3f}'.format(TN[bh_cnt][0] / (bh*bh_test)), ' ', '{:.3f}'.format(FP[bh_cnt][0] / (bh*bh_test)),
-          '{:.3f}'.format(TN[bh_cnt][1] / (bh*bh_test)), ' ', '{:.3f}'.format(FP[bh_cnt][1] / (bh*bh_test)),
-          '{:.3f}'.format(TN[bh_cnt][2] / (bh*bh_test)), ' ', '{:.3f}'.format(FP[bh_cnt][2] / (bh*bh_test)))
-    print('')
-    for bh_cnt in range(0, bh_test):
-        print('bh=', bh_cnt + bh_train + 1)
+    print('TN,FP: ', '{:.3f}'.format(bh*bh_test - outside_train[0]), ' ', '{:.3f}'.format(outside_train[0]),
+          '{:.3f}'.format(bh*bh_test - outside_train[1]), ' ', '{:.3f}'.format(outside_train[1]),
+          '{:.3f}'.format(bh*bh_test - outside_train[2]), ' ', '{:.3f}'.format(outside_train[2]))
+
+    print('TNR,FPR: ', '{:.3f}'.format((bh*bh_test - outside_train[0]) / (bh*bh_test)), ' ', '{:.3f}'.format(outside_train[0] / (bh*bh_test)),
+          '{:.3f}'.format((bh*bh_test - outside_train[1]) / (bh*bh_test)), ' ', '{:.3f}'.format(outside_train[1] / (bh*bh_test)),
+          '{:.3f}'.format((bh*bh_test - outside_train[2]) / (bh*bh_test)), ' ', '{:.3f}'.format(outside_train[2] / (bh*bh_test)))
+    print('End of training data')
+
+    # test result with "before" data (TN and FP)
+    for bh_cnt in range(0, bh_test-5):
+        print('bh=', bh_cnt + 1)
+        print('TN,FP: ', '{:.3f}'.format(TN[bh_cnt][0]), ' ', '{:.3f}'.format(FP[bh_cnt][0])
+              , ' ', '{:.3f}'.format(TN[bh_cnt][1]), ' ', '{:.3f}'.format(FP[bh_cnt][1])
+              , ' ', '{:.3f}'.format(TN[bh_cnt][2]), ' ', '{:.3f}'.format(FP[bh_cnt][2]))
+
+        print('TNR,FPR: ', '{:.3f}'.format(TN[bh_cnt][0] / bh), ' ', '{:.3f}'.format(FP[bh_cnt][0] / bh)
+              , ' ', '{:.3f}'.format(TN[bh_cnt][1] / bh), ' ', '{:.3f}'.format(FP[bh_cnt][1] / bh)
+              , ' ', '{:.3f}'.format(TN[bh_cnt][2] / bh), ' ', '{:.3f}'.format(FP[bh_cnt][2] / bh))
+    print('End of Before water')
+
+    # test result with "after" data (TP and FN)
+    for bh_cnt in range(bh_test-5, bh_test):
+        print('bh=', bh_cnt + 1)
         print('TP,FN: ', '{:.3f}'.format(TP[bh_cnt][0]), ' ', '{:.3f}'.format(FN[bh_cnt][0])
               , ' ', '{:.3f}'.format(TP[bh_cnt][1]), ' ', '{:.3f}'.format(FN[bh_cnt][1])
               , ' ', '{:.3f}'.format(TP[bh_cnt][2]), ' ', '{:.3f}'.format(FN[bh_cnt][2]))
@@ -150,17 +172,24 @@ for fidx in range(0, np.size(sr_list)):
         print('TPR,FNR: ', '{:.3f}'.format(TP[bh_cnt][0] / bh), ' ', '{:.3f}'.format(FN[bh_cnt][0] / bh)
               , ' ', '{:.3f}'.format(TP[bh_cnt][1] / bh), ' ', '{:.3f}'.format(FN[bh_cnt][1] / bh)
               , ' ', '{:.3f}'.format(TP[bh_cnt][2] / bh), ' ', '{:.3f}'.format(FN[bh_cnt][2] / bh))
+    print('End of After water')
 
     f_name = 'result2_' + Sub_run + '.csv'
     with open(f_name, mode='w',newline='') as file:
         writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        writer.writerow(['TNR,FPR:', '{:.3f}'.format(TN[bh_cnt][0] / (bh*bh_test)), '{:.3f}'.format(FP[bh_cnt][0] / (bh*bh_test)),
-                             '{:.3f}'.format(TN[bh_cnt][1] / (bh*bh_test)), '{:.3f}'.format(FP[bh_cnt][1] / (bh*bh_test)),
-                             '{:.3f}'.format(TN[bh_cnt][2] / (bh*bh_test)), '{:.3f}'.format(FP[bh_cnt][2] / (bh*bh_test))])
-        writer.writerow(['TPR,FNR:'])
+        writer.writerow(['TNR,FPR_train:', '{:.3f}'.format((bh*bh_test - outside_train[0]) / (bh*bh_test)), '{:.3f}'.format(outside_train[0] / (bh*bh_test)),
+                             '{:.3f}'.format((bh*bh_test - outside_train[1]) / (bh*bh_test)), '{:.3f}'.format(outside_train[1] / (bh*bh_test)),
+                             '{:.3f}'.format((bh*bh_test - outside_train[2]) / (bh*bh_test)), '{:.3f}'.format(outside_train[2] / (bh*bh_test))])
 
-        for bh_cnt in range(0, bh_test):
+        writer.writerow(['TNR,FPR_test:'])
+        for bh_cnt in range(0, bh_test-5):
+            writer.writerow([bh_cnt + bh_train + 1, '{:.3f}'.format(TN[bh_cnt][0] / bh), '{:.3f}'.format(FP[bh_cnt][0] / bh),
+                                 '{:.3f}'.format(TN[bh_cnt][1] / bh), '{:.3f}'.format(FP[bh_cnt][1] / bh),
+                                 '{:.3f}'.format(TN[bh_cnt][2] / bh), '{:.3f}'.format(FP[bh_cnt][2] / bh)])
+
+        writer.writerow(['TPR,FNR:'])
+        for bh_cnt in range(bh_test-5, bh_test):
             writer.writerow([bh_cnt + bh_train + 1, '{:.3f}'.format(TP[bh_cnt][0] / bh), '{:.3f}'.format(FN[bh_cnt][0] / bh),
                                  '{:.3f}'.format(TP[bh_cnt][1] / bh), '{:.3f}'.format(FN[bh_cnt][1] / bh),
                                  '{:.3f}'.format(TP[bh_cnt][2] / bh), '{:.3f}'.format(FN[bh_cnt][2] / bh)])
